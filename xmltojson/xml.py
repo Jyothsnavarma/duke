7215f4b7db8d741edc57652f1f335249d99fa54b
Jyothsnavarma <144342281+Jyothsnavarma@users.noreply.github.com>
2023-09-14 18:11:16 +0530

    Update xml.py

--- xmltojson/xml.py
+++ xmltojson/xml.py
@@ -1,21 +1,28 @@
-
-
 import xmltodict
-
-import pprint
-
 import json
 
  
 
-with open('person.xml') as fd:
+def convert_xml_to_json(xml_file_path):
+    try:
+        with open(xml_file_path, 'r') as xml_data:
+            # Parse the XML data into a dictionary using xmltodict
+            data_dict = xmltodict.parse(xml_data.read())
 
-   my_dict = xmltodict.parse(fd.read())
+            # Convert the dictionary to JSON 
+            json_data = json.dumps(data_dict, indent=4)
 
-fd.close()
+            return json_data
+    except FileNotFoundError:
+        return "Error: The specified XML file was not found."
+
 
  
 
-pp = json.dumps(my_dict)
+if __name__ == "__main__":
+    xml_file_path = input('Enter XML file: ')
 
-print(pp)
+    # Calling the function to convert the XML to JSON
+    json_data = convert_xml_to_json(xml_file_path)
+
+    print(json_data)
