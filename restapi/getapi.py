4271a542ddbc88fa6c765f8bbcf686e789d65d2c
Jyothsnavarma <144342281+Jyothsnavarma@users.noreply.github.com>
2023-09-14 17:44:10 +0530

    Update getapi.py

--- restapi/getapi.py
+++ restapi/getapi.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # Sample data (replace with your own data source)
 items = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"},
-    {"id": 3, "name": "Item 3"},
+    {"num": 1, "name": "RAJA", "class": 5 },
+    {"num": 2, "name": "RANI", "class": 2 },
+    {"num": 3, "name": "RAMA", "class": 3 },
 ]
 
 # Endpoint to get a list of all items
@@ -15,13 +15,13 @@ def get_items():
     return jsonify(items)
 
 # Endpoint to get a specific item by ID
-@app.route('/api/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = next((item for item in items if item['id'] == item_id), None)
+@app.route('/api/items/<int:item_num>', methods=['GET'])
+def get_item(item_num):
+    item = next((item for item in items if item['num'] == item_num), None)
     if item:
         return jsonify(item)
     else:
         return jsonify({"message": "Item not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
